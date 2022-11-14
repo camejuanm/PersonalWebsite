70c2b762dae02a272c0c5697845e0618d1efe331
Juan <camejuanm@gmail.com>
2022-11-14 22:32:47 +0700

    more fixes, still having problem with image

--- webpack.config.js
+++ webpack.config.js
@@ -6,52 +6,51 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-    mode: "development",
-    entry: './src/js/main.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devtool: 'eval-source-map',
-    devServer: {
-        static: path.resolve(__dirname, 'dist'),
-        port: 8080,
-        hot: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(s(a|c)ss)$/,
-                use: ['style-loader','css-loader', 'sass-loader']
-             },
+  mode: "development",
+  entry: './src/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s(a|c)ss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
 
-             {
-                test: /\.(gif|png|jpe?g)$/,
-                use: [
-                    {
-                      loader: 'file-loader',
-                      options: {
-                        name: '[name].[ext]',
-                        outputPath: 'assets/images/'
-                      }
-                    }
-                  ]
-             },
-
-             {
-                test:/\.html$/,
-                use: [
-                  'html-loader'
-                ]
-              },
-
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            }
+          }
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            inject: 'body',
-            template: './src/html/index.html',
-            filename: 'index.html'
-        })
+      },
+
+      {
+        test: /\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
+
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: './src/html/index.html',
+      filename: 'index.html'
+    })
+  ]
 };
