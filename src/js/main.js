cfcfc1da878d712c45f4f47d066e295d5bedda87
Juan <camejuanm@gmail.com>
2022-12-20 21:59:44 +0700

    minor content update

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,18 @@
 
 import { app as firebase } from './firebase-config'
 import '../scss/styles.scss'
+import '../css/styles.css'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 require('bootstrap')
+const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+darkModeMediaQuery.addEventListener('change', (e) => {
+    const darkModeOn = e.matches;
+    if (darkModeOn) {
+        document.getElementsByTagName("nav").classList.add('navbar-dark');
+    } else document.getElementsByTagName("nav").classList.remove('navbar-dark');
+    
+    
+    //   console.log(`Dark mode is ${darkModeOn ? '🌒 on' : '☀️ off'}.`);
+    //   darkModeOn ? document.getElementsByTagName('figcaption').innerHTML = "Don't be naugthy, dark mode is not supported!" : document.getElementsByTagName('figcaption').innerHTML = "Good boi!"
+});
